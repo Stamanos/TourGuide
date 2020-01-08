fa3a32d9722d55d4a7060db4f3d6802655183886
Manos Stamatakis <themagicsport@gmail.com>
2020-01-08 12:51:23 +0200

    test comment

--- TourGuideIntro/app/src/main/java/com/example/tourguideintro/MapsActivity.java
+++ TourGuideIntro/app/src/main/java/com/example/tourguideintro/MapsActivity.java
@@ -33,6 +33,7 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         mMap = googleMap;
 
         // Add a marker in Sydney and move the camera
+        //test comment
         LatLng sydney = new LatLng(-34, 151);
         mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
         mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
